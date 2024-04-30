cb7a7bd79695cf85b66fed802b3676950bc5e071
beca_utd <gitlab.utd@linguaserve.com>
2024-04-30 14:10:25 +0200

    Pagina principal responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,31 @@ import { signikaNegative } from "@/components/fonts/signika";
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center ">
-      <div className="flex   justify-around">
-        <div className="flex-col w-2/4 ">
-          <h1 className={`${lugrasimo.className} text-4xl  md:text-8xl md:leading-normal`}>
+    <main className="flex flex-col items-center justify-center min-h-screen w-full p-4">
+      <div className="w-full">
+        <div className="text-center mb-12 md:mb-6">
+          <h1 className={`${lugrasimo.className} text-6xl md:text-8xl md:leading-normal`}>
             <strong>View<span className='text-red-600'>Soul</span></strong>
           </h1>
-          <h2 className={`${signikaNegative.className},mt-3 text-lg md:text-2xl`}>VIEWSOUL te ayuda a comunicarte y compartir con las personas que forman parte de tu vida</h2>
         </div>
-        <div className="flex justify-center content-center">
-
-          <Link
-            href="/auth/login"
-            className="flex items-center gap-5 self-center rounded-lg bg-red-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-red-400 md:text-base"
-          >
-            <span>Inicia Sesion</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-
-
+        <div className="flex flex-col md:flex-row md:justify-around md:w-full">
+          <div className="text-center md:w-2/4">
+            <h2 className={`${signikaNegative.className} text-lg md:text-2xl`}>
+              VIEWSOUL te ayuda a comunicarte y compartir con las personas que forman parte de tu vida
+            </h2>
+          </div>
+          <div className="flex justify-center mt-8 md:mt-0">
+            <Link
+              href="/auth/login"
+              className="flex items-center gap-5 self-center rounded-lg bg-red-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-red-400 md:text-base"
+            >
+              <span>Inicia Sesion</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          </div>
         </div>
       </div>
     </main>
+
+
   );
 }
