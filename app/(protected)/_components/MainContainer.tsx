1c47fb24ae6410f3b2bce3b5ee139ab2be9573a1
beca_utd <gitlab.utd@linguaserve.com>
2024-05-27 11:05:53 +0200

    Verdaderos cambios en los git

--- app/(protected)/_components/MainContainer.tsx
+++ app/(protected)/_components/MainContainer.tsx
@@ -15,7 +15,7 @@ export default function ManinContainer({ children }: { children: React.ReactNode
 
     const title = pageTitles.find((page) => page.url === firstPath)?.title || "";
     return (
-<<<<<<< HEAD
+
         <div className="flex-grow  overflow-auto md:mt-4 rounded bg-gradient-to-r from-black to-red-950">
 
             <div className="w-full   ">
@@ -23,15 +23,8 @@ export default function ManinContainer({ children }: { children: React.ReactNode
                     <h1 className={`text-3xl  text-white ml-2`}>{title}</h1>
                 </div>
                 <hr className=" w-full h-1 border-none bg-gradient-to-r from-white to-red-950" />
-=======
-        <div className="flex-grow p-6 overflow-auto md:p-12 md:mt-4 rounded bg-gradient-to-r from-black to-red-950">
 
-            <div className="w-full   ">
-                <div className="flex w-full items-center ">
-                    <h1 className={`text-3xl  text-white  ${lugrasimo.className}`}>{title}</h1>
-                </div>
-                    <hr className=" w-full h-1 border-none bg-gradient-to-r from-white to-red-950"/>
->>>>>>> 0ca575fabacd1e313d22ada8c2d28a8345a6f0b9
+
 
                 <div className="mt-5 flex w-full justify-center ">
                     {children}
--- app/(protected)/_components/user/postProfile.tsx
+++ app/(protected)/_components/user/postProfile.tsx
@@ -26,33 +26,29 @@ export default function PostProfile({ id }: PostProfileProps) {
 
     return (
         <div className="w-full mt-4 h-full text-white bg-transparent">
-<<<<<<< HEAD
-=======
-        <h2 className="text-xl font-semibold mb-4">Publicaciones</h2>
->>>>>>> 0ca575fabacd1e313d22ada8c2d28a8345a6f0b9
-        {userPosts.map((post) => (
-            <div key={post.id} className=" rounded-lg shadow-md mb-4 border border-white">
-                <MiniCardProfile id={id} />
-                <div className="flex flex-col border-t-2">
-                    <p className="text-lg">{post.postText}</p>
-                    {post.postImage && (
-                        <div className="flex justify-center mt-2 ">
-                            <Image
-                                src={getImagePath(post.postImage)}
-                                alt="Post Image"
-                                width={500}
-                                height={500}
-                                priority={true}
-                                className="rounded"
-                            />
-                        </div>
-                    )}
+            {userPosts.map((post) => (
+                <div key={post.id} className=" rounded-lg shadow-md mb-4 border border-white">
+                    <MiniCardProfile id={id} />
+                    <div className="flex flex-col border-t-2">
+                        <p className="text-lg">{post.postText}</p>
+                        {post.postImage && (
+                            <div className="flex justify-center mt-2 ">
+                                <Image
+                                    src={getImagePath(post.postImage)}
+                                    alt="Post Image"
+                                    width={500}
+                                    height={500}
+                                    priority={true}
+                                    className="rounded"
+                                />
+                            </div>
+                        )}
+                    </div>
+                    <p className="text-gray-300 text-sm mt-2">
+                        Publicado el: {new Date(post.createdAt).toLocaleString()}
+                    </p>
                 </div>
-                <p className="text-gray-300 text-sm mt-2">
-                    Publicado el: {new Date(post.createdAt).toLocaleString()}
-                </p>
-            </div>
-        ))}
-    </div>
-);
+            ))}
+        </div>
+    );
 }
