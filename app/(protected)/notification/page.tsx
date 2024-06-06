d318bb262656196574f7809e39eb4a40139f68f9
beca_utd <gitlab.utd@linguaserve.com>
2024-06-06 17:51:42 +0200

    refactor: Remove unused dialog components and update notification page layout

--- app/(protected)/notification/page.tsx
+++ app/(protected)/notification/page.tsx
@@ -25,20 +25,11 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+
 import { TrashIcon } from '@heroicons/react/24/outline';
 import { deleteNotification } from "@/actions/deleteNotification";
 import { useNotifications } from "../hooks/useNotifications";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+
 
 export default function NotificationPage() {
   const [friendProfiles, setFriendProfiles] = useState<ProfileUser[]>([]);
@@ -115,45 +106,7 @@ export default function NotificationPage() {
                         <p className="text-base md:text-md">{friend.name} {friend.lastname} te dio like en tu post</p>
                         <p className="text-xs md:text-sm">@{friend.nickname ? friend.nickname : "No nickname"}</p>
                       </div>
-                      <Dialog>
-                        <DialogTrigger asChild>
-                          <Button variant="outline">Ver post</Button>
-                        </DialogTrigger>
-                        <DialogContent className="sm:max-w-[425px]">
-                          <DialogHeader>
-                            <DialogTitle>Edit profile</DialogTitle>
-                            <DialogDescription>
-                              Make changes to your profile here. Click save when you're done.
-                            </DialogDescription>
-                          </DialogHeader>
-                          <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                              <Label htmlFor="name" className="text-right">
-                                Name
-                              </Label>
-                              <Input
-                                id="name"
-                                defaultValue="Pedro Duarte"
-                                className="col-span-3"
-                              />
-                            </div>
-                            <div className="grid grid-cols-4 items-center gap-4">
-                              <Label htmlFor="username" className="text-right">
-                                Username
-                              </Label>
-                              <Input
-                                id="username"
-                                defaultValue="@peduarte"
-                                className="col-span-3"
-                              />
-                            </div>
-                          </div>
-                          <DialogFooter>
-                            <Button type="submit">Save changes</Button>
-                          </DialogFooter>
-                        </DialogContent>
-                      </Dialog>
-
+                    
                       <AlertDialog>
                         <AlertDialogTrigger><TrashIcon className="w-5 hover:bg-red-600 rounded-lg" /></AlertDialogTrigger>
                         <AlertDialogContent>
